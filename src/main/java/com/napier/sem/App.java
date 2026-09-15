d891d6c3efb097531be00b5d7897686c38137cc6
ThoonTH <40870395@live.napier.ac.uk>
2026-09-15 16:38:13 +0630

    Fixed Network bridge, own docker image created

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,7 +7,7 @@ import org.bson.Document;
 
 public class App
 {
-    public static void main(String[] args)
+    static void main()
     {
         //Connect to mongoDB
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
